3f6a61210b2b59a151ddeb60b6907e3893707b03
Kumar Saurabh <india.kumar.saurabh@hotmail.com>
2024-03-25 20:40:56 +0530

    Update CORS configuration in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,13 @@ import runQuery from './router/runQuery';
 import bodyParser from 'body-parser';
 
 const app = express();
-app.use(cors()); // add this line to enable all CORS requests
+
+// app.use(cors()); // add this line to enable all CORS requests
+app.use(cors({
+    origin: 'https://tuf-show-code-snippets.vercel.app' // replace with your origin
+  }));
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
